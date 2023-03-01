4ebf12b5950169abdb81f254a846d31f582dbf8e
alexdilion <alexdilion@protonmail.com>
2023-03-01 17:39:38 +0000

    Add toggleGrid function and new button style

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,68 +1,87 @@
 let gridContainer = document.getElementById("grid-container");
-let tiles
+let tiles;
 
 const DEFAULT_SETTINGS = {
-  "gridSize": 16,
-  "selectedTool": "brush",
-  "backgroundColor": "#ffffff",
-  "gridLinesEnabled": true
-}
+    gridSize: 16,
+    selectedTool: "brush",
+    backgroundColor: "#ffffff",
+    gridLinesEnabled: true,
+};
 
 const SETTING_LIMITS = {
-  "maxGridSize": 64,
-  "minGridSize": 1
-}
+    maxGridSize: 64,
+    minGridSize: 1,
+};
 
 const TOOLS = {
-  "brush": paint,
-  "eraser": erase
-}
+    brush: paint,
+    eraser: erase,
+};
 
 let settings = DEFAULT_SETTINGS;
 let mouseDown = false;
-let selectedColor = "#1b1b1b"
-let currentTool = "brush"
-let backgroundColor = DEFAULT_SETTINGS["backgroundColor"]
+let selectedColor = "#1b1b1b";
+let currentTool = "brush";
+let backgroundColor = DEFAULT_SETTINGS["backgroundColor"];
 
 function generateGrid() {
-  let numCells = Math.pow(settings["gridSize"], 2);
-  
-  // Makes the cells square-y
-  gridContainer.style.gridTemplateColumns = `repeat(${settings["gridSize"]}, 1fr)`
+    let numCells = Math.pow(settings.gridSize, 2);
 
-  for (let i = 0; i < numCells; i++) {
-    let tile = document.createElement("div");
-    tile.id = `tile${i}`;
-    tile.classList.add("tile");
-    gridContainer.appendChild(tile);
-  }
+    // Makes the cells square-y
+    gridContainer.style.gridTemplateColumns = `repeat(${settings["gridSize"]}, 1fr)`;
 
-  tiles = document.querySelectorAll(".tile");
+    for (let i = 0; i < numCells; i++) {
+        let tile = document.createElement("div");
+        tile.id = `tile${i}`;
+        tile.classList.add("tile");
+
+        if (settings.gridLinesEnabled) {
+            tile.classList.add("grid-lines");
+        }
+
+        gridContainer.appendChild(tile);
+    }
+
+    tiles = document.querySelectorAll(".tile");
 }
 
 function paint(tile) {
-  tile.style.backgroundColor = selectedColor;
+    tile.style.backgroundColor = selectedColor;
 }
 
 function erase(tile) {
-  tile.style.backgroundColor = backgroundColor
+    tile.style.backgroundColor = backgroundColor;
 }
 
-generateGrid()
+function toggleGrid() {
+    settings.gridLinesEnabled = !settings.gridLinesEnabled
+
+    if (settings.gridLinesEnabled) {
+        tiles.forEach(tile => {
+            tile.classList.add("grid-lines");
+        })
+    } else {
+        tiles.forEach(tile => {
+            tile.classList.remove("grid-lines");
+        })
+    }
+}
+
+generateGrid();
 
 tiles.forEach((tile) => {
-  tile.addEventListener("mouseover", () => {
-    if (mouseDown) {
-      TOOLS[currentTool](tile)
-    }
-  })
-})
+    tile.addEventListener("mouseover", () => {
+        if (mouseDown) {
+            TOOLS[currentTool](tile);
+        }
+    });
+});
 
 document.addEventListener("mousedown", (e) => {
-  mouseDown = true;
-  TOOLS[currentTool](e.target)
-})
+    mouseDown = true;
+    TOOLS[currentTool](e.target);
+});
 
 document.addEventListener("mouseup", () => {
-  mouseDown = false;
-})
+    mouseDown = false;
+});
